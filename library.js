4cc36f31f16da0f99af5ea2c81f57d7f8c5583a6
kwizera-bonheur123 <kbonheur123@gmail.com>
2024-01-15 09:46:54 +0200

    add the feature to r=edit the book

--- library.js
+++ library.js
@@ -24,7 +24,7 @@
         return JSON.parse(localStorage.getItem("books"));
     }
     const books = getItems();
-    console.log(books);
+   
     var display_wrapper = document.getElementById("display_wrapper");
 
     for(let i = 0; i < books.length; i++){
@@ -33,16 +33,16 @@
         newDiv.innerHTML = `
         <div class = "display">
                     <div class="content">
-                        <div id="nomero">${id}</div>
+                        <div id="nomero">${books[i].id}</div>
                         <p id="title1">${books[i].title}</p>
                         <p id="aunthor">${books[i].author}</p>
                         <p id="page">${books[i].number}</p>
                     </div>
                     <div class="icons">
-                        <div id="edit" onclick(editBook(${i}))>
+                        <div id="edit" onclick = "editBook(${i})">
                             <i class="fa-solid fa-pen-to-square"></i>
                         </div>
-                        <button id="delete" onclick="deleteBook(${i})"><i class="fa-solid fa-trash"></i></button>
+                        <button id="delete" onclick="deleteBook(${books[i].id})"><i class="fa-solid fa-trash"></i></button>
                     </div>
                 </div>
             <div>
@@ -50,26 +50,41 @@
         display_wrapper.appendChild(newDiv);
     }
 
+    const author = document.getElementById("anthor");
+    const title = document.getElementById("title");
+    const number = document.getElementById("number");
+
     document.getElementById("send").addEventListener("click",(e)=>{
         e.preventDefault();
-        const author = document.getElementById("anthor").value;
-        const title = document.getElementById("title").value;
-        const number = document.getElementById("number").value;
         let books = getItems();
         console.log(books.length);
         const newBook = {
-            id:books.length,
-            author:author,
-            title:title,
-            number:number
+            id:books.length+1,
+            author:author.value,
+            title:title.value,
+            number:number.value
         }
         books.push(newBook);
         addItem(books)
         window.location.reload();
     })
 function deleteBook(id){
-    const books = getItems();
     const newArray = books.filter((book) => book.id !== id);
-    addItem(newArray)
+    addItem(newArray);
     window.location.reload();
 }
+function editBook(index){
+    document.getElementById("editbtn").style.display = "block";
+    document.getElementById("send").style.display = "none";
+    author.value = books[index].author;
+    title.value = books[index].title;
+    number.value = books[index].number;
+    document.getElementById("saveIndex").value = index;
+}
+document.getElementById("editbtn").addEventListener("click",function(e){
+    e.preventDefault();
+    const index = document.getElementById("saveIndex").value
+    books[index] = {...books[index],author:author.value,title:title.value,number:number.value };
+    addItem(books);
+    window.location.reload();
+})
